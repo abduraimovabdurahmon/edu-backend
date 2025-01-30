f3ca650a36e2bc9b8fd8df22948f4e257782d150
Abduraxmon <geoinfocomuchun@gmail.com>
2025-01-30 15:35:50 +0500

    add database enviroment

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,19 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "abdurakhmon",
-    password: "jomiy007",
-    database: "edu",
+    type: process.env.DATASOURCE_TYPE as any, 
+    host: process.env.DATASOURCE_HOST || "localhost",
+    port: parseInt(process.env.DATASOURCE_PORT || "5432"),
+    username: process.env.DATASOURCE_USER,
+    password: process.env.DATASOURCE_PASS,
+    database: process.env.DATASOURCE_NAME || "postgres",
     synchronize: true,
     logging: false,
     entities: [__dirname + "/entity/*.ts"],
     migrations: [],
     subscribers: [],
-})
+});
